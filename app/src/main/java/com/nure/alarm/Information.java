50cd96de8d183770a1a675a2ac691eca4050b6b7
MagisterFelix <felix.vlad.14@gmail.com>
2022-07-29 22:34:49 +0300

    Extend information data class

--- app/src/main/java/com/nure/alarm/Information.java
+++ app/src/main/java/com/nure/alarm/Information.java
@@ -2,9 +2,13 @@ package com.nure.alarm;
 
 public class Information {
     private boolean status;
+    private int alarmHour;
+    private int alarmMinute;
 
-    public Information(boolean status) {
+    public Information(boolean status, int alarmHour, int alarmMinute) {
         this.status = status;
+        this.alarmHour = alarmHour;
+        this.alarmMinute = alarmMinute;
     }
 
     public boolean getStatus() {
@@ -13,5 +17,21 @@ public class Information {
 
     public void setStatus(boolean status) {
         this.status = status;
+    }
+
+    public int getAlarmHour() {
+        return alarmHour;
+    }
+
+    public void setAlarmHour(int alarmHour) {
+        this.alarmHour = alarmHour;
+    }
+
+    public int getAlarmMinute() {
+        return alarmMinute;
+    }
+
+    public void setAlarmMinute(int alarmMinute) {
+        this.alarmMinute = alarmMinute;
     }
 }
