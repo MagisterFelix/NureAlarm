21ca439045a5aba9bd595c30dd0f8abaeecf8cc6
MagisterFelix <felix.vlad.14@gmail.com>
2022-07-29 20:45:07 +0300

    Add information data class

---
+++ app/src/main/java/com/nure/alarm/Information.java
@@ -0,0 +1,17 @@
+package com.nure.alarm;
+
+public class Information {
+    private boolean status;
+
+    public Information(boolean status) {
+        this.status = status;
+    }
+
+    public boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+}
