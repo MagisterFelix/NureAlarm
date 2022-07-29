be063fbdc8620f7a887185444f3110593648c67b
MagisterFelix <felix.vlad.14@gmail.com>
2022-07-29 20:46:53 +0300

    Add status switcher

--- app/src/main/java/com/nure/alarm/MainActivity.java
+++ app/src/main/java/com/nure/alarm/MainActivity.java
@@ -3,12 +3,33 @@ package com.nure.alarm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+
+import com.google.android.material.switchmaterial.SwitchMaterial;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Information information;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        information = FileManager.getInfo(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+
+        SwitchMaterial switchMaterial = menu.findItem(R.id.switchStatus).getActionView().findViewById(R.id.switchStatus);
+        switchMaterial.setChecked(information.getStatus());
+        switchMaterial.setOnCheckedChangeListener((compoundButton, isChecked) -> {
+            information.setStatus(isChecked);
+            FileManager.updateInfo(this, information);
+        });
+
+        return true;
     }
 }
