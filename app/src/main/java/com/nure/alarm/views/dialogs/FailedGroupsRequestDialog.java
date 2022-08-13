6c4eaf8f8180e427e12937a4f82a59ac7230473a
MagisterFelix <felix.vlad.14@gmail.com>
2022-08-14 01:51:18 +0300

    Add modals with explanations

---
+++ app/src/main/java/com/nure/alarm/views/dialogs/FailedGroupsRequestDialog.java
@@ -0,0 +1,23 @@
+package com.nure.alarm.views.dialogs;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+import com.nure.alarm.R;
+
+public class FailedGroupsRequestDialog extends AppCompatDialogFragment {
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Failed groups request")
+                .setMessage(R.string.failed_groups_request_message)
+                .setPositiveButton("ok", null);
+        return builder.create();
+    }
+}
--- app/src/main/java/com/nure/alarm/views/dialogs/PermissionDialog.java
+++ app/src/main/java/com/nure/alarm/views/dialogs/PermissionDialog.java
@@ -1,4 +1,4 @@
-package com.nure.alarm.views;
+package com.nure.alarm.views.dialogs;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
