9cd8060d29d1456b00a70687c32d6a7f75fcff3a
MagisterFelix <felix.vlad.14@gmail.com>
2023-02-15 04:47:31 +0200

    Change method for setting alarm work

--- app/src/main/java/com/nure/alarm/core/work/AlarmWorkManager.java
+++ app/src/main/java/com/nure/alarm/core/work/AlarmWorkManager.java
@@ -5,20 +5,15 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 
-import com.nure.alarm.views.MainActivity;
-
 public class AlarmWorkManager {
 
     public static void setAlarmWork(Context context, long time) {
-        PendingIntent mainActivity = PendingIntent.getActivity(context, 0,
-                new Intent(context, MainActivity.class),
-                PendingIntent.FLAG_IMMUTABLE | PendingIntent.FLAG_UPDATE_CURRENT);
         PendingIntent alarmWorkerReceiver = PendingIntent.getBroadcast(context, 0,
                 new Intent(context, AlarmWorkerReceiver.class),
                 PendingIntent.FLAG_IMMUTABLE | PendingIntent.FLAG_UPDATE_CURRENT);
 
         AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-        alarmManager.setAlarmClock(new AlarmManager.AlarmClockInfo(time, mainActivity), alarmWorkerReceiver);
+        alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, time, alarmWorkerReceiver);
     }
 
     public static void cancelAlarmWork(Context context) {
