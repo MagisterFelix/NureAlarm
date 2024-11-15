9feaf9e9b47855fd3bbe1378dd696b47ea3c8533
MagisterFelix <vladyslav.lapin.14@gmail.com>
2024-11-15 17:13:26 +0200

    Improve alarm logic

--- app/src/main/java/com/nure/alarm/views/AlarmActivity.java
+++ app/src/main/java/com/nure/alarm/views/AlarmActivity.java
@@ -1,13 +1,15 @@
 package com.nure.alarm.views;
 
+import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Bundle;
-import android.os.Handler;
 import android.os.PowerManager;
+import android.view.MotionEvent;
 import android.view.WindowManager;
-import android.widget.ImageButton;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -18,16 +20,23 @@ import com.nure.alarm.core.Alarm;
 import com.nure.alarm.core.managers.ContextManager;
 import com.nure.alarm.core.managers.FileManager;
 import com.nure.alarm.core.models.Information;
+import com.nure.alarm.core.models.LessonsType;
+import com.nure.alarm.core.work.AlarmWorkerReceiver;
 
 import org.json.JSONException;
 
 public class AlarmActivity extends AppCompatActivity {
 
     private static final String ROTATION_ANIMATION = "rotation";
+    private static final String ALPHA_ANIMATION = "alpha";
+    private static final String TRANSLATION_ANIMATION = "translationX";
 
-    private Runnable runnable;
-    private final Handler handler = new Handler();
+    private FrameLayout swipeButtonContainer;
+    private ImageView swipeButton;
 
+    private float dX;
+
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,38 +66,87 @@ public class AlarmActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
-        ImageView firstCircle = findViewById(R.id.first_circle);
-        ImageView secondCircle = findViewById(R.id.second_circle);
-
-        runnable = () -> {
-            firstCircle.animate().scaleX(3f).scaleY(3f).alpha(0f).setDuration(1000).withEndAction(() -> {
-                firstCircle.setScaleX(1f);
-                firstCircle.setScaleY(1f);
-                firstCircle.setAlpha(1f);
-            });
-
-            secondCircle.animate().scaleX(3f).scaleY(3f).alpha(0f).setDuration(700).withEndAction(() -> {
-                secondCircle.setScaleX(1f);
-                secondCircle.setScaleY(1f);
-                secondCircle.setAlpha(1f);
-            });
-
-            handler.postDelayed(runnable, 1500);
-        };
-        runnable.run();
-
-        ImageButton dismissAlarmButton = findViewById(R.id.dismiss_alarm_button);
-        dismissAlarmButton.setOnClickListener(view -> {
-            handler.removeCallbacks(runnable);
-
-            Alarm.stopAlarm(getApplicationContext());
-            AlarmClockActivity.updateActivity(getApplicationContext(), false);
-            finish();
+        swipeButtonContainer = findViewById(R.id.swipe_button_container);
+        swipeButton = findViewById(R.id.swipe_button);
+        swipeButton.setOnTouchListener((v, event) -> {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    dX = v.getX() - event.getRawX();
+                    return true;
+                case MotionEvent.ACTION_MOVE:
+                    float newX = event.getRawX() + dX;
+                    if (newX < 0) {
+                        newX = 0;
+                    } else if (newX + swipeButton.getWidth() > swipeButtonContainer.getWidth()) {
+                        newX = swipeButtonContainer.getWidth() - swipeButton.getWidth();
+                    }
+                    v.setX(newX);
+                    if (newX + (float) swipeButton.getWidth() / 2 < swipeButtonContainer.getWidth() * 0.5f) {
+                        swipeButton.setImageResource(R.drawable.ic_alarm_off);
+                    } else if (newX + (float) swipeButton.getWidth() / 2 > swipeButtonContainer.getWidth() * 0.5f) {
+                        swipeButton.setImageResource(R.drawable.ic_alarm_set);
+                    }
+                    return true;
+                case MotionEvent.ACTION_UP:
+                    if (v.getX() + (double) v.getWidth() / 2 > swipeButtonContainer.getWidth() * 0.75) {
+                        reset();
+                    } else if (v.getX() + (double) v.getWidth() / 2 < swipeButtonContainer.getWidth() * 0.25) {
+                        dismiss();
+                    } else {
+                        resetButton();
+                    }
+                    return true;
+            }
+            return false;
         });
+
+        ImageView leftArrow1, leftArrow2, rightArrow1, rightArrow2;
+        leftArrow1 = findViewById(R.id.left_arrow_1);
+        leftArrow2 = findViewById(R.id.left_arrow_2);
+        rightArrow1 = findViewById(R.id.right_arrow_1);
+        rightArrow2 = findViewById(R.id.right_arrow_2);
+        setupArrowAnimation(leftArrow1, 0, -50f);
+        setupArrowAnimation(leftArrow2, 800, -50f);
+        setupArrowAnimation(rightArrow1, 0, 50f);
+        setupArrowAnimation(rightArrow2, 800, 50f);
     }
 
     @Override
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(ContextManager.getLocaleContext(base));
+    }
+
+    private void setupArrowAnimation(ImageView arrow, long startDelay, float translation) {
+        ObjectAnimator alphaAnimation = ObjectAnimator.ofFloat(arrow, ALPHA_ANIMATION, 1f, 0f);
+        alphaAnimation.setDuration(1600);
+        alphaAnimation.setStartDelay(startDelay);
+        alphaAnimation.setRepeatCount(ValueAnimator.INFINITE);
+
+        ObjectAnimator translationAnimation = ObjectAnimator.ofFloat(arrow, TRANSLATION_ANIMATION, 0f, translation);
+        translationAnimation.setDuration(1600);
+        translationAnimation.setStartDelay(startDelay);
+        translationAnimation.setRepeatCount(ValueAnimator.INFINITE);
+
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.playTogether(alphaAnimation, translationAnimation);
+        animatorSet.start();
+    }
+
+    private void dismiss() {
+        Alarm.stopAlarm(getApplicationContext());
+        AlarmClockActivity.updateActivity(getApplicationContext(), false);
+        finish();
+    }
+
+    private void reset() {
+        Alarm.stopAlarm(getApplicationContext());
+        AlarmClockActivity.updateActivity(getApplicationContext(), true);
+        AlarmWorkerReceiver.startWork(getApplicationContext(), LessonsType.TODAY_NEAREST, true);
+        finish();
+    }
+
+    private void resetButton() {
+        swipeButton.animate().x((float) (swipeButtonContainer.getWidth() - swipeButton.getWidth()) / 2).setDuration(150).start();
+        swipeButton.setImageResource(R.drawable.ic_stop);
     }
 }
